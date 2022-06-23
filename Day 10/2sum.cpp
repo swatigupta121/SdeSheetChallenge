fef0667a5376a1b7046510636732cffcb1c11c1c
Swati Gupta <73897489+swatigupta121@users.noreply.github.com>
2022-06-23 21:08:18 +0530

    Create 2sum.cpp

---
+++ Day 10/2sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        vector<int> res;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find(target-nums[i])!=mp.end())   //O(log n) [for unordered map find it will be O(1)]
+            {
+                res.push_back(mp[target-nums[i]]);
+                res.push_back(i);   // O(1) for vector push_back
+                break;
+            }
+            mp[nums[i]]=i;
+        }
+        return res;
+    }
+};
