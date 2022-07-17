c5a1db702357c5cb7d39eebef4234d433e37c25e
Swati Gupta <73897489+swatigupta121@users.noreply.github.com>
2022-07-17 19:30:02 +0530

    Update Median of 2 sorted arrays.cpp

--- Day 29/Median of 2 sorted arrays.cpp
+++ Day 29/Median of 2 sorted arrays.cpp
@@ -100,29 +100,39 @@ public:
         //     return ans;
         // return ans/2;
         
+        //O(log n)
         int m=nums1.size(), n=nums2.size();
         if(m>n)
-          return findMedianSortedArrays(nums2,nums1);
+            return findMedianSortedArrays(nums2,nums1);
         
-      int low=0,high=m,medianPos=((m+n)+1)/2;
-      while(low<=high) {
-          int cut1 = (low+high)>>1;
-          int cut2 = medianPos - cut1;
-
-          int l1 = (cut1 == 0)? INT_MIN:nums1[cut1-1];
-          int l2 = (cut2 == 0)? INT_MIN:nums2[cut2-1];
-          int r1 = (cut1 == m)? INT_MAX:nums1[cut1];
-          int r2 = (cut2 == n)? INT_MAX:nums2[cut2];
-
-          if(l1<=r2 && l2<=r1) {
-              if((m+n)%2 != 0)
-                  return max(l1,l2);
-              else 
-                  return (max(l1,l2)+min(r1,r2))/2.0;
-          }
-          else if(l1>r2) high = cut1-1;
-          else low = cut1+1;
-      }
-      return 0.0;
+        int low=0,high=m,median_pos=(m+n+1)/2;
+        while(low<=high)
+        {
+            int cut1=(low+high)/2;
+            int cut2=median_pos-cut1;
+            
+            int l1=(cut1==0) ? INT_MIN : nums1[cut1-1];
+            int l2=(cut2==0) ? INT_MIN : nums2[cut2-1];
+            
+            int r1=(cut1==m) ? INT_MAX : nums1[cut1];
+            int r2=(cut2==n) ? INT_MAX : nums2[cut2];
+            
+            if(l1<=r2 && l2<=r1)
+            {
+                if((n+m)%2==0)
+                    return (max(l1,l2)+min(r1,r2))/2.0;
+                else
+                    return max(l1,l2);
+            }
+            else if(l1>r2)
+            {
+                high=cut1-1;
+            }
+            else
+            {
+                low=cut1+1;
+            }
+        }
+        return 0.0;
     }
 };
