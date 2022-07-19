ebbed49c91185d29360b5f315f5fc18c9e36d50f
Swati Gupta <73897489+swatigupta121@users.noreply.github.com>
2022-07-19 21:56:40 +0530

    Update 2sum.cpp

--- Day 10/2sum.cpp
+++ Day 10/2sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        
         map<int,int> mp;
         vector<int> res;
         for(int i=0;i<nums.size();i++)
